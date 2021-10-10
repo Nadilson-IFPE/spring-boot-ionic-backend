883e4d7a7a7764f0a70ddbccbdf28b82ee5d5c52
Nadilson <nadilson@protonmail.com>
2021-10-10 05:06:17 -0300

    Projeto criado (Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB)

---
+++ src/test/java/com/nadilson/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadilson.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
