7bc78a6967a4584dec6240996222562f9b9b091a
Nadilson <nadilson@protonmail.com>
2021-10-12 05:13:19 -0300

    ItemPedido e ItemPedidoPK (Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB)

--- src/main/java/com/nadilson/cursomc/domain/Pedido.java
+++ src/main/java/com/nadilson/cursomc/domain/Pedido.java
@@ -2,6 +2,8 @@ package com.nadilson.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -10,12 +12,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-public class Pedido implements Serializable{
+public class Pedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -36,6 +39,10 @@ public class Pedido implements Serializable{
 	@JoinColumn(name="endereco_id")
 	private Endereco enderecoDeEntrega;
 	
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
+
+
 	public Pedido() {
 		
 	}
@@ -87,6 +94,16 @@ public class Pedido implements Serializable{
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
+	
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
+	}
+	
+	
 
 	@Override
 	public int hashCode() {
@@ -113,9 +130,4 @@ public class Pedido implements Serializable{
 		return true;
 	}
 	
-	
-	
-	
-	
-
 }
