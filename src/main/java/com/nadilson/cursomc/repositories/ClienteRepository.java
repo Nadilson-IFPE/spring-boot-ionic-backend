c2156a29692f7a9dc51724e456716fb43891da76
Nadilson <nadilson@protonmail.com>
2021-10-12 01:31:56 -0300

    Cliente, TipoCLiente, telefones e endereços (Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB)

---
+++ src/main/java/com/nadilson/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.nadilson.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nadilson.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
