afc418aee9b5fe739799d87165445920ab56bc9e
Nadilson <nadilson@protonmail.com>
2021-10-13 02:20:08 -0300

    Inserindo uma nova Categoria com POST (Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB)

--- src/main/java/com/nadilson/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/nadilson/cursomc/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.nadilson.cursomc.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nadilson.cursomc.domain.Categoria;
 import com.nadilson.cursomc.services.CategoriaService;
@@ -31,6 +35,16 @@ public class CategoriaResource {
 		
 		
 		//return lista;
+	}
+	
+	// CRUD
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
