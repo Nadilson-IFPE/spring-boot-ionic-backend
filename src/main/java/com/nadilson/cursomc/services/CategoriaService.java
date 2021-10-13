be77df57ec8433bb643f192d3f31ecd6c21b05cb
Nadilson <nadilson@protonmail.com>
2021-10-13 04:58:26 -0300

    Paginação com parâmetros opcionais na requisição (Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB)

--- src/main/java/com/nadilson/cursomc/services/CategoriaService.java
+++ src/main/java/com/nadilson/cursomc/services/CategoriaService.java
@@ -5,6 +5,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.stereotype.Service;
 
 import com.nadilson.cursomc.domain.Categoria;
@@ -19,15 +23,10 @@ public class CategoriaService {
 	private CategoriaRepository repo;
 	
 	public Categoria find(Integer id) {
-	//	Categoria obj = repo.findOne(id);
-	//	return obj;
-		
-		//Optional<Categoria> obj = repo.findById(id); 
-		Optional<Categoria> obj = repo.findById(id); 
-		
+	    Optional<Categoria> obj = repo.findById(id); 
+
 		return obj.orElseThrow(() -> new ObjectNotFoundException( 
 		 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
-		//return obj.orElse(null); 
 	}
 
 	public Categoria insert(Categoria obj) {
@@ -43,7 +42,7 @@ public class CategoriaService {
 	public void delete(Integer id) {
 		find(id);
 		try {
-		repo.deleteById(id);
+		  repo.deleteById(id);
 		} catch (DataIntegrityViolationException e) {
 		  	throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
 		}
@@ -51,5 +50,11 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), 
+				orderBy);		
+		return repo.findAll(pageRequest);
 	}
 }
