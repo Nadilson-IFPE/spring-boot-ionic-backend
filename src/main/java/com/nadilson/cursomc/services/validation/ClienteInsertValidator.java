c0862817774d42167e019be45789d2a2299cc0b8
Nadilson <nadilson@protonmail.com>
2021-10-14 01:19:16 -0300

    Validação customizada: email não repetido na inserção de Cliente (Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB)

--- src/main/java/com/nadilson/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/nadilson/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.nadilson.cursomc.domain.Cliente;
 import com.nadilson.cursomc.domain.enums.TipoCliente;
 import com.nadilson.cursomc.dto.ClienteNewDTO;
+import com.nadilson.cursomc.repositories.ClienteRepository;
 import com.nadilson.cursomc.resources.exception.FieldMessage;
 import com.nadilson.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -34,7 +42,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 				!BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido!"));
 		}
-
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "E-mail já existente!"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldname())
