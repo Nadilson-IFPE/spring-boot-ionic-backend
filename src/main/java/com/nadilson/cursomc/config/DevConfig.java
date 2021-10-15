4ce6da701597f4c1dfca5f5acc014eb9009d7ae0
Nadilson <nadilson@protonmail.com>
2021-10-14 23:48:49 -0300

    Criando profile de desenvolvimento (Spring Boot, Hibernate, REST, Ionic, JWT, S3, MySQL, MongoDB)

---
+++ src/main/java/com/nadilson/cursomc/config/DevConfig.java
@@ -0,0 +1,33 @@
+package com.nadilson.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.nadilson.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	// Estratégia para impedir que o BD seja recriado a cada execução do aplicativo.
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		if (!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateDatabase();
+		return true;
+	}
+}
