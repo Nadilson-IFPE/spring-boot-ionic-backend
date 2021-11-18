a7c57dff516d4a69710867943885c2f26e469db0
Nadilson <nadilson@protonmail.com>
2021-11-18 17:34:37 -0300

    Documentação com Swagger - Parte 1

--- src/main/java/com/nadilson/cursomc/config/SecurityConfig.java
+++ src/main/java/com/nadilson/cursomc/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -51,6 +52,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			"/clientes",
 			"/auth/forgot/**"
 	};
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**", "/configuration/**",
+		"/swagger-ui.html", "/webjars/**");
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
